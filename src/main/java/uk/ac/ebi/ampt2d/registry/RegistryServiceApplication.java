0e508fca6c60b8be61870b97945fc5e749642ca6
Zhicheng-Liu <zl@ebi.ac.uk>
2018-03-20 13:18:42 +0000

    Add building war file

--- src/main/java/uk/ac/ebi/ampt2d/registry/RegistryServiceApplication.java
+++ src/main/java/uk/ac/ebi/ampt2d/registry/RegistryServiceApplication.java
@@ -19,11 +19,18 @@ package uk.ac.ebi.ampt2d.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class RegistryServiceApplication {
+public class RegistryServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RegistryServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RegistryServiceApplication.class);
     }
 }
