def8dea466f97c3bdce9756d60a8232a4a0dd4df
selva <selva@ebi.ac.uk>
2018-04-10 17:46:55 +0100

    test base path for url

--- src/main/java/uk/ac/ebi/ampt2d/registry/SwaggerConfiguration.java
+++ src/main/java/uk/ac/ebi/ampt2d/registry/SwaggerConfiguration.java
@@ -71,7 +71,7 @@ public class SwaggerConfiguration {
                 .pathProvider(new RelativePathProvider(servletContext) {
                     @Override
                     public String getApplicationBasePath() {
-                        return "/ega/ampt2d/registry/";
+                        return "/" + super.getApplicationBasePath();
                     }
                 })
                 .select()
