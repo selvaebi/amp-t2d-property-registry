c8db218173b024a97cfeca2193920310d4fe1fd4
selva <selva@ebi.ac.uk>
2018-11-29 11:13:54 +0000

    test correction

--- src/test/java/uk/ac/ebi/ampt2d/registry/PropertyRegistryServiceApplicationTests.java
+++ src/test/java/uk/ac/ebi/ampt2d/registry/PropertyRegistryServiceApplicationTests.java
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -33,6 +34,8 @@ import uk.ac.ebi.ampt2d.registry.service.mail.MailService;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -42,8 +45,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(properties = "security.enabled=true")
+@SpringBootTest(properties = {"security.enabled=true","spring.jpa.hibernate.ddl-auto=none"})
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = AFTER_CLASS)
 public class PropertyRegistryServiceApplicationTests {
 
     @Autowired
--- src/test/java/uk/ac/ebi/ampt2d/registry/RegistryUpdateNotificationFailure.java
+++ src/test/java/uk/ac/ebi/ampt2d/registry/RegistryUpdateNotificationFailure.java
@@ -22,9 +22,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.transaction.Transactional;
+
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = {"mail.notify=true", "spring.mail.host=invalid_host"})
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = BEFORE_CLASS)
 public class RegistryUpdateNotificationFailure {
 
     @Autowired
